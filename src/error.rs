7fea1ed34e2bff0a3798591ced5c3a1f7ea46e3c
Aaron Griffin <aaron@sevco.io>
2021-05-19 11:32:18 -0700

    Simplified errors, add std::error::Error interop

--- src/error.rs
+++ src/error.rs
@@ -1,77 +1,31 @@
-use std::convert::From;
-use std::fmt::Display;
+use thiserror::Error;
 
 /// Errors for DataDogLogger
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum DataDogLoggerError {
     /// Error that can happen if DataDog URL is not valid
-    UrlParsingError(url::ParseError),
+    #[error("{0}")]
+    UrlParsingError(#[from] url::ParseError),
     /// Error that can happen during serialization of message
-    MessageSerializationError(serde_json::Error),
+    #[error("{0}")]
+    MessageSerializationError(#[from] serde_json::Error),
     /// I/O error
-    IoError(std::io::Error),
+    #[error("{0}")]
+    IoError(#[from] std::io::Error),
     /// Logger configuration error
+    #[error("{0}")]
     ConfigError(String),
     /// Generic error container
+    #[error("{0}")]
     OtherError(String),
     /// Http logger error
-    HttpError(attohttpc::Error),
+    #[error("{0}")]
+    HttpError(#[from] attohttpc::Error),
     /// Error that can happen during DataDogLogger initialization with log
-    LogIntegrationError(log::SetLoggerError),
+    #[error("{0}")]
+    LogIntegrationError(#[from] log::SetLoggerError),
     /// Http error in non blocking client
     #[cfg(feature = "nonblocking")]
-    AsyncHttpError(reqwest::Error),
-}
-
-impl Display for DataDogLoggerError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DataDogLoggerError::UrlParsingError(e) => write!(f, "{}", e),
-            DataDogLoggerError::MessageSerializationError(e) => write!(f, "{}", e),
-            DataDogLoggerError::IoError(e) => write!(f, "{}", e),
-            DataDogLoggerError::ConfigError(e) => write!(f, "{}", e),
-            DataDogLoggerError::OtherError(e) => write!(f, "{}", e),
-            DataDogLoggerError::HttpError(e) => write!(f, "{}", e),
-            DataDogLoggerError::LogIntegrationError(e) => write!(f, "{}", e),
-            #[cfg(feature = "nonblocking")]
-            DataDogLoggerError::AsyncHttpError(e) => write!(f, "{}", e),
-        }
-    }
-}
-
-impl From<url::ParseError> for DataDogLoggerError {
-    fn from(e: url::ParseError) -> Self {
-        DataDogLoggerError::UrlParsingError(e)
-    }
-}
-
-impl From<serde_json::Error> for DataDogLoggerError {
-    fn from(e: serde_json::Error) -> Self {
-        DataDogLoggerError::MessageSerializationError(e)
-    }
-}
-
-impl From<std::io::Error> for DataDogLoggerError {
-    fn from(e: std::io::Error) -> Self {
-        DataDogLoggerError::IoError(e)
-    }
-}
-
-impl From<attohttpc::Error> for DataDogLoggerError {
-    fn from(e: attohttpc::Error) -> Self {
-        DataDogLoggerError::HttpError(e)
-    }
-}
-
-impl From<log::SetLoggerError> for DataDogLoggerError {
-    fn from(e: log::SetLoggerError) -> Self {
-        DataDogLoggerError::LogIntegrationError(e)
-    }
-}
-
-#[cfg(feature = "nonblocking")]
-impl From<reqwest::Error> for DataDogLoggerError {
-    fn from(e: reqwest::Error) -> Self {
-        DataDogLoggerError::AsyncHttpError(e)
-    }
+    #[error("{0}")]
+    AsyncHttpError(#[from] reqwest::Error),
 }
